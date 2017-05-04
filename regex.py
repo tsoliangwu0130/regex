6794e4fc5cbd630f89ef2e6424cb4e5d2025e417
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-03 21:57:24 -0700

    Update regex

--- regex.py
+++ regex.py
@@ -4,3 +4,9 @@ example_string = '''
 Jessica is 15 years old, and Daniel is 27 years old.
 Edward is 97 years old, and his grandfather, Oscar, is 102.
 '''
+
+ages = re.findall(r'\d{1,3}', example_string)
+names = re.findall(r'[A-Z][a-z]*', example_string)
+
+print(ages)
+print(names)
